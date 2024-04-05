b6ce897a433f52b71abaef0c0fd8f2207b10eff4
billy <billyrangel@hotmail.com>
2024-04-05 14:51:18 -0600

    add example dataTable

---
+++ IESMvc/Controllers/InstitucionController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IESMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IESMvc.Controllers
+{
+    public class InstitucionController : Controller
+    {
+
+        private IList<Institucion> instituciones = new List<Institucion>
+        {
+            new Institucion { InstitucionID=1, Nombre="UniParana", Direccion="Parana"},
+            new Institucion { InstitucionID=2, Nombre="UniSanta", Direccion="Santa Catarina"},
+            new Institucion { InstitucionID=3, Nombre = "UniSãoPaulo", Direccion="São Paulo"},            new Institucion { InstitucionID=4, Nombre = "UniSulgrandense", Direccion="Rio Grande do Sul"},            new Institucion { InstitucionID=5, Nombre = "UniCarioca", Direccion="Rio de janeiro"}
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituciones);
+        }
+    }
+}
---
+++ IESMvc/Models/Institucion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IESMvc.Models
+{
+    public class Institucion
+    {
+        public int InstitucionID { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+    }
+}
+
